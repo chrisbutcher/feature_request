a7675d2ca514fd87e186491dfab4745b349ada24
Alfred Wong <alfred.wong@patientordersets.com>
2013-11-26 13:53:11 -0500

    CSAPI-462 Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MongoPlatform::Application.config.secret_token = 'bbd71007577db46e4a81201cdbadc39e7e7926f4b6246542acea4a914978576ef9dae16c0ba08424d520e3c28cecea87d4a617cbb8ee8d783702bdaacce455e6'
